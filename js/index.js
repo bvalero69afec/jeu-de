fe7d647ea6f33048419b0aad72a1e9fe5814b04c
Benjamin Valero <bvalero69afec@proton.me>
2025-11-05 12:47:26 +0100

    Add comments

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+//Constants
 const NUMBER_OF_PLAYERS = 5;
 const MAX_ROLL = 5;
 
+//Initialize players
 const players = [];
 for (let i = 0; i < NUMBER_OF_PLAYERS; ++i) {
   players[i] = {
@@ -12,63 +14,77 @@ for (let i = 0; i < NUMBER_OF_PLAYERS; ++i) {
 
 const gameContainer = document.querySelector("#game-container");
 
+//Update the text of the score element of the player card with its current score
 function updatePlayerCardScore(playerCardScore, playerId) {
   playerCardScore.textContent = players[playerId].score.toString();
 }
 
+//Update the text of the roll button element of the player card with its current number of rolls
 function updatePlayerCardRollButton(playerCardRollButton, playerId) {
   playerCardRollButton.textContent = `Lancer (${ players[playerId].roll })`;
 }
 
+//Construct all player card elements and add them in the game container
 for (let i = 0; i < NUMBER_OF_PLAYERS; ++i) {
   const playerCard = document.createElement("div");
   playerCard.classList.add("player-card");
-  playerCard.setAttribute("data-player", i.toString());
+  playerCard.setAttribute("data-player", i.toString()); //Identify the card element with the player id
   gameContainer.appendChild(playerCard);
 
+  //Construct the player name element of the card
   const playerCardName = document.createElement("div");
   playerCardName.classList.add("player-card-name");
   playerCardName.textContent = players[i].name;
   playerCard.appendChild(playerCardName);
 
+  //Construct the body element of the card
   const playerCardBody = document.createElement("div");
   playerCardBody.classList.add("player-card-body");
   playerCard.appendChild(playerCardBody);
 
+  //Construct the player score element of the card body
   const playerCardScore = document.createElement("div");
   playerCardScore.classList.add("player-card-score");
   updatePlayerCardScore(playerCardScore, i);
   playerCardBody.appendChild(playerCardScore);
 
+  //Construct the roll button element of the card
   const playerCardRollButton = document.createElement("button");
   playerCardRollButton.classList.add("player-card-roll-button");
   updatePlayerCardRollButton(playerCardRollButton, i);
   playerCard.appendChild(playerCardRollButton);
 }
 
+//Get a random integer number between two numbers, inclusive
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
+//Add a global click event listener on the game container for all roll buttons
 gameContainer.addEventListener("click", function (event) {
   const target = event.target;
+  //Check if a roll button element is clicked inside the game container
   if (target.classList.contains("player-card-roll-button")) {
-    const playerCard = target.closest(".player-card");
-    const playerId = Number.parseInt(playerCard.dataset.player);
+    const playerCard = target.closest(".player-card"); //Retrieve the player card element of the roll button
+    const playerId = Number.parseInt(playerCard.dataset.player); //Retrieve the player id from the card element
 
+    //Roll a die and update the new player total score
     const rollScore = getRandomIntInclusive(1, 10);
     players[playerId].score += rollScore;
     const playerCardScore = playerCard.querySelector(".player-card-score");
     updatePlayerCardScore(playerCardScore, playerId);
 
+    //Increment the number of rolls of the player and update its text
     players[playerId].roll++;
     updatePlayerCardRollButton(target, playerId);
+    //Disable the button if we reached the roll limit
     if (players[playerId].roll >= MAX_ROLL) {
       target.disabled = true;
     }
 
+    //Check if all players reached the roll limit to end the game
     let gameEnded = true;
     for (const player of players) {
       if (player.roll < MAX_ROLL) {
@@ -80,6 +96,7 @@ gameContainer.addEventListener("click", function (event) {
     if (gameEnded) {
       const leaderboard = [];
 
+      //Add player name and score entries in the leaderboard
       for (let i = 0; i < NUMBER_OF_PLAYERS; ++i) {
         const player = players[i];
         leaderboard[i] = {
@@ -88,6 +105,7 @@ gameContainer.addEventListener("click", function (event) {
         };
       }
 
+      //Sort the leaderboard by score in descending order. If tie, sort lexicographically by name
       leaderboard.sort(function (a, b) {
         if (a.score === b.score) {
           return a.name.localeCompare(b.name);
@@ -95,6 +113,8 @@ gameContainer.addEventListener("click", function (event) {
         return b.score - a.score;
       });
 
+      //Calculate the rank (competition ranking system, for example "1,2,2,4") of each player
+      //Track the previous rank and score while iterating on the sorted leaderboard to determine the next rank
       let previousRank = null;
       let previousScore = null;
       for (let i = 0; i < leaderboard.length; ++i) {
@@ -102,10 +122,11 @@ gameContainer.addEventListener("click", function (event) {
         const score = leaderboardEntry.score;
 
         let rank;
+        //Check if it's not a tie with the player above
         if (score !== previousScore) {
-          rank = i + 1;
+          rank = i + 1; //Set the rank to the player index from the sorted leaderboard + 1
         } else {
-          rank = previousRank;
+          rank = previousRank; //Set the rank to the same as the player above since it's a tie
         }
 
         leaderboardEntry.rank = rank;
@@ -114,9 +135,10 @@ gameContainer.addEventListener("click", function (event) {
         previousScore = score;
       }
 
-      const leaderboardJson = JSON.stringify(leaderboard);
-      localStorage.setItem("leaderboard", leaderboardJson);
+      const leaderboardJson = JSON.stringify(leaderboard); //Serialize the leaderboard array to JSON
+      localStorage.setItem("leaderboard", leaderboardJson); //Save the leaderboard json inside the local storage of the browser
 
+      //Wait and redirect to the leaderboard page
       const delayMs = 2 * 1000;
       setTimeout(function () {
         window.location.assign("leaderboard.html#leaderboard");
--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,8 @@
 const leaderboardElem = document.querySelector("#leaderboard");
 
+//Construct the element of a leaderboard entry and add it to the leaderboard element
 function addLeaderboardEntryElem(leaderboardEntry) {
+  //Choose a specific style class and rank text depending on the player rank
   let leaderboardEntryElemClass;
   let leaderboardEntryRankElemText;
   switch (leaderboardEntry.rank) {
@@ -25,25 +27,29 @@ function addLeaderboardEntryElem(leaderboardEntry) {
   leaderboardEntryElem.classList.add(leaderboardEntryElemClass);
   leaderboardElem.appendChild(leaderboardEntryElem);
 
+  //Construct the rank element of the leaderboard entry
   const leaderboardEntryRankElem = document.createElement("span");
   leaderboardEntryRankElem.classList.add("leaderboard-entry-rank");
   leaderboardEntryRankElem.textContent = leaderboardEntryRankElemText;
   leaderboardEntryElem.appendChild(leaderboardEntryRankElem);
 
+  //Construct the player name element of the leaderboard entry
   const leaderboardEntryNameElem = document.createElement("span");
   leaderboardEntryNameElem.classList.add("leaderboard-entry-name");
   leaderboardEntryNameElem.textContent = leaderboardEntry.name;
   leaderboardEntryElem.appendChild(leaderboardEntryNameElem);
 
+  //Construct the player score element of the leaderboard entry
   const leaderboardEntryScoreElem = document.createElement("div");
   leaderboardEntryScoreElem.classList.add("leaderboard-entry-score");
   leaderboardEntryScoreElem.textContent = leaderboardEntry.score.toString();
   leaderboardEntryElem.appendChild(leaderboardEntryScoreElem);
 }
 
-const leaderboardJson = localStorage.getItem('leaderboard');
+const leaderboardJson = localStorage.getItem('leaderboard'); //Get the leaderboard json saved in the local storage of the browser
 if (leaderboardJson !== null) {
-  const leaderboard = JSON.parse(leaderboardJson);
+  const leaderboard = JSON.parse(leaderboardJson); //Deserialize the leaderboard json into an array
+  //Construct the leaderboard entry elements
   for (const leaderboardEntry of leaderboard) {
     addLeaderboardEntryElem(leaderboardEntry);
   }
